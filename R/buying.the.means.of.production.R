8836d8ab0512a58ba573d962d6914429d5c6f50d
Robert Lee <rlee35@uwo.ca>
2015-09-04 17:34:39 -0700

    Initial script.

---
+++ R/buying.the.means.of.production.R
@@ -0,0 +1,61 @@
+# setwd("D:/Programming/buying.the.means.of.production")
+
+# Canada
+
+# 2012 Canadian GDP in billions of current (2015) Canadian $
+1831.2
+# Wages, Salaries & Supplementary Labour Income (same units)
+926.2 # from: http://www.international.gc.ca/economist-economiste/statistics-statistiques/data-facts-fiches/Facts_fiches-Canada_Quarterly-trimestriel_Ec-Ind.aspx?lang=eng
+
+# Canadian publically traded stocks (billions of 2012 US $)
+2016 # from: https://www.quandl.com/collections/economics/stock-market-capitalization-by-country
+
+# 2012 CAD/USD = 1.000230 ~ 1
+# from: http://www.canadianforex.ca/forex-tools/historical-rate-tools/yearly-average-rates
+
+# Canadian inflation 2012 to 2015: 
+1.0477 # http://www.bankofcanada.ca/rates/related/inflation-calculator/
+
+# current wages/salaries/income to 2012 dollars: 
+926.2 / 1.0477
+
+# Years of salary to buy all publically traded Canadian stocks:
+2016/ (926.2/1.0477)
+
+# US proportion of firms publically held by revenue in 2007
+0.636 # from : https://www.sba.gov/sites/default/files/rs385tot_0.pdf, pg 11
+
+# Approximate years of salary to buy all Canadian firms:
+2016/ (926.2/1.0477) / 0.636
+# Assume 20% savings, 
+2016/ (926.2/1.0477) / 0.636 / .2
+# ~18 years to take over the means of production.  
+# Call it 20.  
+# But less if profits from company ownership are used to fund further purchases.
+
+
+# US
+# Share of GDI going to wages and salaries in 2011
+.443 # from: https://research.stlouisfed.org/fred2/series/W269RE1A156NBEA
+
+# GDI in 2011 (billions of $)
+15556.3
+# GDI in 2012 (billions of $)
+16358.5 # from: https://research.stlouisfed.org/fred2/series/W269RE1A156NBEA
+
+# Price of publically traded stocks in USA, 2012 billions of $
+18668
+
+# Years to buy publically traded stocks in USA, 2011 GDI, 2011 labour share, 2012 market cap
+18668/ (15556.3 * .443)
+# Years to buy publically traded stocks in USA, 2012 GDI, 2011 labour share, 2012 market cap
+18668/ (16358.5 * .443)
+
+# US proportion of firms publically held by revenue in 2007
+0.636 # from : https://www.sba.gov/sites/default/files/rs385tot_0.pdf, pg 11
+
+# Approximate years of salary to buy all US firms:
+18668/ (15556.3 * .443) / 0.636
+# Assume a 20% savings rate
+18668/ (15556.3 * .443) / 0.636 / .2
+# ~21 years to take over the means of production
